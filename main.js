07ee6dde61bb58ba07610aec2fffb3415641c713
manudana11 <manueldanapallardo@gmail.com>
2024-04-12 16:48:48 +0200

    creamos las variables para el crear spa

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hola proyecto Quiz!')
+const home = document.getElementById('home');
+const quiz = document.getElementById('quiz');
+const stats = document.getElementById('stats');
+const homeNav = document.getElementById('homeNav');
+const quizNav = document.getElementById('quizNav');
+const statsNav = document.getElementById('statsNav');
+

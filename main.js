5173f6490f9ca2ac5363b6160bacd4f1deb26afc
manudana11 <manueldanapallardo@gmail.com>
2024-04-12 17:09:05 +0200

    barra de navegación configutada con exito por el momento

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ const homeNav = document.getElementById('homeNav');
 const quizNav = document.getElementById('quizNav');
 const statsNav = document.getElementById('statsNav');
 
+const hideViews = () => {
+    home.classList.add('hiden');
+    quiz.classList.add('hiden');
+    stats.classList.add('hiden');
+}
+
+const showHome = () => {
+    hideViews();
+    home.classList.remove('hiden');
+}
+
+const showQuiz = () => {
+    hideViews();
+    quiz.classList.remove('hiden');
+}
+
+const showStats = () => {
+    hideViews();
+    stats.classList.remove('hiden');
+}
+
+homeNav.addEventListener('click', showHome);
+quizNav.addEventListener('click', showQuiz);
+statsNav.addEventListener('click', showStats);

ff9f8f1fb1c45be865229121cc19f988572ab583
manudana11 <manueldanapallardo@gmail.com>
2024-04-13 14:19:41 +0200

    algunos cambios y configurado el boton de start

--- main.js
+++ main.js
@@ -3,38 +3,62 @@
 const home = document.getElementById('home');
 const quiz = document.getElementById('quiz');
 const stats = document.getElementById('stats');
-const homeNav = document.getElementById('homeNav');
-const quizNav = document.getElementById('quizNav');
-const statsNav = document.getElementById('statsNav');
+const homeNav = document.getElementById('home-nav');
+const quizNav = document.getElementById('quiz-nav');
+const statsNav = document.getElementById('stats-nav');
+const welcomeQuiz = document.getElementById('welcome-quiz');
+const btnStart = document.getElementById('btn-start');
+const btnNext = document.getElementById('btn-next');
+const questionsContainerElement = document.getElementById('questions-container');
+const questionElement = document.getElementById('questions');
+const answersButtonsElement = document.getElementById('answers-buttons');
 const API_URL = 'https://opentdb.com/api.php?amount=10';
+let questionsApi = [];
+let currentQuestionIndex;
+console.log(home, quiz,stats,homeNav,quizNav,statsNav,welcomeQuiz,btnStart,btnNext,questionElement,answersButtonsElement)
 
 // FUNCIONES
 
+axios.get(API_URL)
+.then((res) => {
+    questionsApi = res.data;
+    console.log(questionsApi);
+}).catch((err) => console.log(err));
+
 const hideViews = () => {
-    home.classList.add('hiden');
-    quiz.classList.add('hiden');
-    stats.classList.add('hiden');
+    home.classList.add('hide');
+    quiz.classList.add('hide');
+    stats.classList.add('hide');
 }
 
 const showHome = () => {
     hideViews();
-    home.classList.remove('hiden');
+    home.classList.remove('hide');
 }
 
 const showQuiz = () => {
     hideViews();
-    quiz.classList.remove('hiden');
+    quiz.classList.remove('hide');
 }
 
 const showStats = () => {
     hideViews();
-    stats.classList.remove('hiden');
+    stats.classList.remove('hide');
 }
 
-axios.get(API_URL).then((res) => console.log('prueba1', res.data))
+const startGame = () => {
+    btnStart.classList.add('hide');
+    currentQuestionIndex = 0;
+    questionsContainerElement.classList.remove('hide');
+}
+
+// const showQuestions = (questionsApi) => {
+
+// }
 
 // LLAMAR FUNCIONES
 
 homeNav.addEventListener('click', showHome);
 quizNav.addEventListener('click', showQuiz);
 statsNav.addEventListener('click', showStats);
+btnStart.addEventListener('click', startGame);
